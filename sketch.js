11fd0504d39b59eb06a120dfca5953ef20964876
RigoEaaa <rigo@eaaa.dk>
2025-02-24 10:04:19 +0100

    New branch

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 function setup() {
   createCanvas(400, 400);
+  background(220);
+  
 }
 
 function draw() {
-  background(220);
+  //background(220);
+  // Draw a circle that follows the mouse
+  fill(255, 0, 0); // Set the fill color to red
+  ellipse(mouseX, mouseY, 50, 50); // Draw the circle at the mouse position
 }
+
+
